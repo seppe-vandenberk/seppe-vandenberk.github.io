55b33672a2854a12f91ef737d512334936020288
SeppeV-2158896 <seppe.vandenberk@uhasselt.be>
2025-12-08 14:34:04 +0100

    update base index

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/my-website/",
   plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
